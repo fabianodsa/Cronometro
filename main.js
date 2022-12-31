3b10fc3be70e629e2586c72da95b1830f7fa7b3b
fabianodsa <fabyanoduarte@hotmail.com>
2022-12-30 22:02:45 -0300

    Criando função para disparar o cronometro

---
+++ main.js
@@ -0,0 +1,5 @@
+const minutesContainer = document.querySelector('#minutes')
+const hourContainer = document.querySelector('#hours')
+const secondsContainer = document.querySelector('#seconds')
+
+console.log(hours)
