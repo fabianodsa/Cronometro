c8c04c32e7e8879a6b62b802e7b326d86d4e829d
fabianodsa <fabyanoduarte@hotmail.com>
2023-01-02 13:46:16 -0300

    Criando função que inicia o cronometro

--- main.js
+++ main.js
@@ -1,8 +1,41 @@
-const minutesTime = document.querySelector('#minutes')
-const hourTime = document.querySelector('#hours')
-const secondsTime = document.querySelector('#seconds')
+var horas = 0;
+var minutos = 0;
+var segundos = 0;
+var cronometro;
 
-function iniciar () {
-    var segundos = 1;
-    document.writeln("já passou " + segundos + " segundos...");
+function start() {
+   cronometro = setInterval(() => {
+    timer();
+    }, 1000);
+} 
+
+function stop() {
+    clearInterval()
+}
+
+function reset() {
+    clearInterval()
+
+    horas = 0;
+    minutos = 0;
+    segundos = 0;
+}
+
+function timer() {
+    segundos++;
+    if (segundos == 60){
+        segundos = 0;
+        minutos++;
+    }
+    if (minutos == 60) {
+        minutos = 0;
+        horas++
+    }
+
+    var format = 
+    (horas < 10 ? "0" + horas : horas) + 
+    ":"
+    (minutos < 10 ? "0" + minutos : minutos) +
+    ":"
+    (segundos < 10 ? "0" + segundos : segundos) +
 }
